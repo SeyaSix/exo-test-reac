a351ed1909f74ca0dc5d230dfe4ae028b2413730
SeyaSix <gomat63@yahoo.fr>
2023-11-07 18:37:26 +0100

    exo react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header.js";
+import Footer from "./Footer";
+import Home from "./home";
+
+function App() {
+  return (
+    <div class="container-test">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home.js
@@ -0,0 +1,12 @@
+import Products from "./products";
+
+function Home() {
+  return (
+    <div>
+      <h2>Découvrez nos super produits !</h2>
+      <Products />
+    </div>
+  );
+}
+
+export default Home;
