ddf04699fc15afeffbb57ef685a49e1a37cfe855
SeyaSix <gomat63@yahoo.fr>
2023-11-08 11:03:27 +0100

    product button

--- src/products.js
+++ src/products.js
@@ -1,11 +1,13 @@
 function Products() {
+
   const ajoutAuPanier = (productName) => {
     console.log(`Ajouté au panier : ${productName}`);
   };
+  
   return (
     <main>
       <article>
-      <div className="product-item">
+      <div class="product-item">
         <h2>Achetez cette magnifique paire de mocassins à glands</h2>
         <button onClick={() => ajoutAuPanier("mocassins à glands")}>
           300€
@@ -14,14 +16,14 @@ function Products() {
       </article>
 
       <article>
-        <div className="product-item">
+        <div class="product-item">
         <h2>Turbo mixeur 3000</h2>
         <button onClick={() => ajoutAuPanier("Turbo mixeur 3000")}>200€</button>
         </div>
       </article>
 
       <article>
-        <div className="product-item">
+        <div class="product-item">
         <h2>Aspire-tout</h2>
 
         <button onClick={() => ajoutAuPanier("Aspire-tout")}>350€</button>
