c641c0551bbc98824df0411a5f92ddca925179a5
SeyaSix <gomat63@yahoo.fr>
2023-11-08 11:04:03 +0100

    product error console

--- src/products.js
+++ src/products.js
@@ -1,35 +1,35 @@
 function Products() {
-
   const ajoutAuPanier = (productName) => {
     console.log(`Ajouté au panier : ${productName}`);
   };
-  
+
   return (
     <main>
       <article>
-      <div class="product-item">
-        <h2>Achetez cette magnifique paire de mocassins à glands</h2>
-        <button onClick={() => ajoutAuPanier("mocassins à glands")}>
-          300€
-        </button>
+        <div class="product-item">
+          <h2>Achetez cette magnifique paire de mocassins à glands</h2>
+          <button onClick={() => ajoutAuPanier("mocassins à glands")}>
+            300€
+          </button>
         </div>
       </article>
 
       <article>
         <div class="product-item">
-        <h2>Turbo mixeur 3000</h2>
-        <button onClick={() => ajoutAuPanier("Turbo mixeur 3000")}>200€</button>
+          <h2>Turbo mixeur 3000</h2>
+          <button onClick={() => ajoutAuPanier("Turbo mixeur 3000")}>
+            200€
+          </button>
         </div>
       </article>
 
       <article>
         <div class="product-item">
-        <h2>Aspire-tout</h2>
+          <h2>Aspire-tout</h2>
 
-        <button onClick={() => ajoutAuPanier("Aspire-tout")}>350€</button>
+          <button onClick={() => ajoutAuPanier("Aspire-tout")}>350€</button>
         </div>
       </article>
-      
     </main>
   );
 }
