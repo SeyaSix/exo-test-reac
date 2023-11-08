132a0ef3156e2e1b795493708d9391a1f8d42fe7
SeyaSix <gomat63@yahoo.fr>
2023-11-08 10:38:11 +0100

    panier

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,18 @@ function Header() {
 
   const isUserLogged = true;
 
+  const itemCart = 0;
+
     return (
       <header>
         <h1>Super ecommerce</h1>
         <nav>
           <ul>
+            {isUserLogged ? <li>nom d'utilisateur</li> : <li>connectez vous</li>}
             <li>Home</li>
             <li>Products</li>
-            {isUserLogged ? <li>nom d'utilisateur</li> : <li>connectez vous</li>
-            }
+            {itemCart > 0 ? <li>Cart : {itemCart}</li> : <li>pas d'item dans le Panier</li>}
+
           </ul>
         </nav>
       </header>
