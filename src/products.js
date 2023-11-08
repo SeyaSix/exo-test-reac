6f9e76d3be64c3d85909e7e155b6824fe13b4f39
SeyaSix <gomat63@yahoo.fr>
2023-11-08 15:39:32 +0100

    liste d'objet et map

--- src/products.js
+++ src/products.js
@@ -1,72 +1,118 @@
-import React, { useState } from 'react';
-
 function Products() {
-  const [showPopup, setShowPopup] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState('');
-
-  const ajouterAuPanier = (productName) => {
-    setSelectedProduct(productName);
-    setShowPopup(true);
-  };
-
-  const closePopup = () => {
-    setShowPopup(false);
-  };
-
-  const productFromDb = [
-    "Mocassins", "Charentaise", "Talon"
+  const productsFromDb = [
+    {
+      title: "scotch ",
+      price: 12,
+      isPublished: true,
+    },
+    {
+      title: "Aspirateur",
+      price: 299,
+      isPublished: true,
+    },
+    {
+      title: "Contre Plaqué",
+      price: 120,
+      isPublished: true,
+    },
+    {
+      title: "Sangles",
+      price: 25,
+      isPublished: true,
+    },
   ];
 
   return (
-
-
-
     <main>
-      <>
-      {productFromDb.map((product)=>{
+      {productsFromDb.map((product) => {
         return (
-        <article>
-          <h2>{product}</h2>
-        </article>
-      );
+          <article>
+            <h2>{product.title}</h2>
+            <p>{product.price} €</p>
+          </article>
+        );
       })}
-      </>
-
-      <article>
-        <div className="product-item">
-          <h2>Achetez cette magnifique paire de mocassins à glands</h2>
-          <button onClick={() => ajouterAuPanier("mocassins à glands")}>
-            300€
-          </button>
-        </div>
-      </article>
-
-      <article>
-        <div className="product-item">
-          <h2>Turbo mixeur 3000</h2>
-          <button onClick={() => ajouterAuPanier("Turbo mixeur 3000")}>
-            200€
-          </button>
-        </div>
-      </article>
-
-      <article>
-        <div className="product-item">
-          <h2>Aspire-tout</h2>
-
-          <button onClick={() => ajouterAuPanier("Aspire-tout")}>350€</button>
-        </div>
-      </article>
-      {showPopup && (
-        <div className="popup">
-          <div className="popup-content">
-            <h2>Ajouté au panier : {selectedProduct}</h2>
-            <button onClick={closePopup}>Fermer</button>
-          </div>
-        </div>
-      )}
     </main>
   );
 }
 
 export default Products;
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+
+// function Products() {
+//   const [showPopup, setShowPopup] = useState(false);
+//   const [selectedProduct, setSelectedProduct] = useState('');
+
+//   const ajouterAuPanier = (productName) => {
+//     setSelectedProduct(productName);
+//     setShowPopup(true);
+//   };
+
+//   const closePopup = () => {
+//     setShowPopup(false);
+//   };
+
+//   const productFromDb = [
+//     "Mocassins", "Charentaise", "Talon"
+//   ];
+
+//   return (
+
+
+
+//     <main>
+//       <>
+//       {productFromDb.map((product)=>{
+//         return (
+//         <article>
+//           <h2>{product}</h2>
+//         </article>
+//       );
+//       })}
+//       </>
+
+//       <article>
+//         <div className="product-item">
+//           <h2>Achetez cette magnifique paire de mocassins à glands</h2>
+//           <button onClick={() => ajouterAuPanier("mocassins à glands")}>
+//             300€
+//           </button>
+//         </div>
+//       </article>
+
+//       <article>
+//         <div className="product-item">
+//           <h2>Turbo mixeur 3000</h2>
+//           <button onClick={() => ajouterAuPanier("Turbo mixeur 3000")}>
+//             200€
+//           </button>
+//         </div>
+//       </article>
+
+//       <article>
+//         <div className="product-item">
+//           <h2>Aspire-tout</h2>
+
+//           <button onClick={() => ajouterAuPanier("Aspire-tout")}>350€</button>
+//         </div>
+//       </article>
+//       {showPopup && (
+//         <div className="popup">
+//           <div className="popup-content">
+//             <h2>Ajouté au panier : {selectedProduct}</h2>
+//             <button onClick={closePopup}>Fermer</button>
+//           </div>
+//         </div>
+//       )}
+//     </main>
+//   );
+// }
+
+// export default Products;
